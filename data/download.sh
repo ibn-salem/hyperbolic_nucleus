10f4f6d0a1863f1a44c4af19bf9ceff12b127e0d
ibn-salem <jonas.ibn-salem@gmx.de>
2017-10-19 12:53:18 +0200

    add download and liftOver of hESC TADs

--- data/download.sh
+++ data/download.sh
@@ -13,3 +13,36 @@ unzip Mifsud2015/E-MTAB-2323.additional.1.zip -d Mifsud2015
 python ./python/reformat_CaptureHiC.py \
   -i Mifsud2015/TS5_GM12878_promoter-promoter_significant_interactions.txt \
   -o Mifsud2015/TS5_GM12878_promoter-promoter_significant_interactions.txt.genePairs
+
+#=======================================================================
+# UCSC lift over chains
+#=======================================================================
+
+mkdir -p UCSC
+wget -P UCSC http://hgdownload.cse.ucsc.edu/goldenPath/hg18/liftOver/hg18ToHg19.over.chain.gz
+gunzip UCSC/*.gz
+
+# download liftOver tool from UCSC:
+mkdir -p bin
+wget -P bin http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/liftOver
+chmod u+x bin/liftOver
+
+
+#=======================================================================
+# TADs in hESC from Dixon et al. 2012
+#=======================================================================
+mkdir -p Dixon2012
+
+C="hESC"
+
+wget -P Dixon2012 http://chromosome.sdsc.edu/mouse/hi-c/${C}.domain.tar.gz 
+# extract and rename
+tar xvfz Dixon2012/${C}.domain.tar.gz -C Dixon2012
+cp Dixon2012/${C}/combined/total.combined.domain Dixon2012/${C}.hg18.bed
+    
+# liftover to hg19
+bin/liftOver \
+      Dixon2012/${C}.hg18.bed \
+      UCSC/hg18ToHg19.over.chain \
+      Dixon2012/${C}.hg18.bed.hg19.bed \
+      Dixon2012/${C}.hg18.bed.hg19.bed_unmapped.bed
